dc6fc567dc5f5c37ca192b67a90ab892eb08e399
AtakanSteven <p2wpV5Lqn7XLEEjaxRsS>
2022-08-07 22:58:43 +0300

    Prettier and eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   root: true,
   env: {
@@ -17,9 +17,12 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "no-console": 2,
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/operator-linebreak': 'off',
   },
 };
